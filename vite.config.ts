b98257e673affb9c8b02883074ec6ec755267e1f
MD Rifat Emam <rifatemam.00@gmail.com>
2026-05-07 00:56:24 +0600

    fix wayfinder import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import wayfinder from '@laravel/vite-plugin-wayfinder';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 
 export default defineConfig({
     plugins: [
